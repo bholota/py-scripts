60ffaec676465efa9c60d2817ee4a08f452534fa
Bartosz Janda <bartosz.janda@gmail.com>
2018-03-18 13:05:48 +0100

    Added adaptive binarization

--- binarization.py
+++ binarization.py
@@ -5,7 +5,7 @@ import os
 
 import numpy as np
 from PIL import Image
-from skimage.filters import threshold_otsu, rank
+from skimage.filters import threshold_otsu, threshold_adaptive, threshold_local, rank
 from skimage.morphology import disk
 from tqdm import tqdm
 
@@ -22,12 +22,23 @@ def convert_to_bw_otsu(original, local=False):
     threshold = rank.otsu(np.array(gray), disk(15)) if local else threshold_otsu(np.array(gray))
     return Image.fromarray(np.uint8(gray >= threshold) * 255)
 
+def convert_to_bw_adaptive(original):
+    gray = original.convert('L')
+    binary_adaptive = threshold_adaptive(np.array(gray), 99, offset=5)
+    # return Image.fromarray(binary_adaptive)
+    return Image.fromarray(np.uint8(binary_adaptive) * 255)
 
-def convert_and_save(file_name, threshold=128, otsu=False, local_otsu=False):
+
+def convert_and_save(file_name, threshold=128, otsu=False, local_otsu=False, adaptive=False):
     try:
         original = Image.open(file_name)
         new_name = file_name.replace('jpg', 'png')
-        bw = convert_to_bw_otsu(original, local_otsu) if otsu else convert_to_bw(original, threshold)
+        if otsu:
+            bw = convert_to_bw_otsu(original, local_otsu)
+        elif adaptive:
+            bw = convert_to_bw_adaptive(original)
+        else:
+            bw = convert_to_bw(original, threshold)
         bw = apply_rotation_from_original(original, bw)
         bw.save(new_name)
         original.close()
@@ -41,13 +52,14 @@ def main():
     parser.add_argument("-t", "--threshold", help="(default 128)", type=int, default=128)
     parser.add_argument("--otsu", action="store_true", help="otsu threshold")
     parser.add_argument("--local", action="store_true", help="works only with --otsu (slower)")
+    parser.add_argument("--adaptive", action="store_true", help="adaptive threshold")
     args = parser.parse_args()
 
     print("Converting to monochrome...")
     listdir = os.listdir(args.dir)
     progress = tqdm(range(len(listdir)), unit="file")
     for file_name in listdir:
-        convert_and_save(f"{args.dir}/{file_name}", args.threshold, args.otsu, args.local)
+        convert_and_save(f"{args.dir}/{file_name}", args.threshold, args.otsu, args.local, args.adaptive)
         progress.update()
     progress.close()
 
