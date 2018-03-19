62723626b4e60589f626c76b5c354872845dfb75
Bartosz Janda <bartosz.janda@gmail.com>
2018-03-19 19:39:58 +0100

    Added using threshold_local instead of threshold_adaptive. Added parameters for threshold_local.

--- binarization.py
+++ binarization.py
@@ -5,7 +5,7 @@ import os
 
 import numpy as np
 from PIL import Image
-from skimage.filters import threshold_otsu, threshold_adaptive, threshold_local, rank
+from skimage.filters import threshold_otsu, threshold_local, rank
 from skimage.morphology import disk
 from tqdm import tqdm
 
@@ -22,21 +22,21 @@ def convert_to_bw_otsu(original, local=False):
     threshold = rank.otsu(np.array(gray), disk(15)) if local else threshold_otsu(np.array(gray))
     return Image.fromarray(np.uint8(gray >= threshold) * 255)
 
-def convert_to_bw_adaptive(original):
+def convert_to_bw_adaptive(original, block_size, offset):
     gray = original.convert('L')
-    binary_adaptive = threshold_adaptive(np.array(gray), 99, offset=5)
-    # return Image.fromarray(binary_adaptive)
+    image = np.array(gray)
+    binary_adaptive = image > threshold_local(image, block_size, offset=offset)
     return Image.fromarray(np.uint8(binary_adaptive) * 255)
 
 
-def convert_and_save(file_name, threshold=128, otsu=False, local_otsu=False, adaptive=False):
+def convert_and_save(file_name, threshold=128, block_size=99, offset=5, otsu=False, local_otsu=False, adaptive=False):
     try:
         original = Image.open(file_name)
         new_name = file_name.replace('jpg', 'png')
         if otsu:
             bw = convert_to_bw_otsu(original, local_otsu)
         elif adaptive:
-            bw = convert_to_bw_adaptive(original)
+            bw = convert_to_bw_adaptive(original, block_size, offset)
         else:
             bw = convert_to_bw(original, threshold)
         bw = apply_rotation_from_original(original, bw)
@@ -50,6 +50,8 @@ def main():
     parser = argparse.ArgumentParser(description="Converts all images in directory to monochrome")
     parser.add_argument("dir", help="Target directory", type=str)
     parser.add_argument("-t", "--threshold", help="(default 128)", type=int, default=128)
+    parser.add_argument("-b", "--block_size", help="(default 99)", type=int, default=99)
+    parser.add_argument("-o", "--offset", help="(default 5)", type=int, default=5)
     parser.add_argument("--otsu", action="store_true", help="otsu threshold")
     parser.add_argument("--local", action="store_true", help="works only with --otsu (slower)")
     parser.add_argument("--adaptive", action="store_true", help="adaptive threshold")
@@ -59,7 +61,7 @@ def main():
     listdir = os.listdir(args.dir)
     progress = tqdm(range(len(listdir)), unit="file")
     for file_name in listdir:
-        convert_and_save(f"{args.dir}/{file_name}", args.threshold, args.otsu, args.local, args.adaptive)
+        convert_and_save(f"{args.dir}/{file_name}", threshold=args.threshold, block_size=args.block_size, offset=args.offset, otsu=args.otsu, local_otsu=args.local, adaptive=args.adaptive)
         progress.update()
     progress.close()
 
